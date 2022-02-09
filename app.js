324a66f2d57bd2c142d6d07a93c6d4b1e4d8c57e
farkhodsoliev <farhodjonsoliyev9@gmail.com>
2022-02-09 19:59:07 +0500

    Countries

---
+++ app.js
@@ -0,0 +1,24 @@
+
+
+        const button = document.querySelector('#button');
+        const body = document.querySelector('body')
+        const button1 = document.querySelector('#button1');
+
+   button.addEventListener("click", function(){
+       document.body.style.background = '#333'
+       
+   })
+   button1.addEventListener("click", function(){
+    document.body.style.background = '#fff'
+});
+button.addEventListener("click", function(){
+    cart_continer.background = '#333'
+    
+})
+
+
+
+
+
+
+
